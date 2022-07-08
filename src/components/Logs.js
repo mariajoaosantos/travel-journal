d48124a5786f064cecd06e93e2ed93488c5c307e
mariajoaosantos <mariajcs18@gmail.com>
2022-07-08 23:58:01 +0100

    Styles added

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -3,14 +3,28 @@ import React from "react";
 export default function Log(props) {
   return (
     <section className="log">
-      <img src={`./images/${props.imageUrl}`}></img>
-      <div className="log--info">
+
+      <img className="log--img" src={`./images/${props.imageUrl}`}></img>
+
+<div className="log--content">
+
+      <div className="log--header">
+        <div className="log--pointer">
         <img src="./images/location-logo.png" className="log--loclogo"></img>
         <span className="log--location">{props.location}</span>
-        <span className="log--title">{props.title}</span>
-        <span className="log--date">{props.startDate} - {props.endDate}</span>
-        <p className="log--description">{props.description}</p>
+        </div>
+        <a href={props.googleMapsUrl} className="log--link" >View on Google Maps</a>
       </div>
+
+      <div className="log--details">
+          <span className="log--title">{props.title}</span>
+          <span className="log--date">
+            {props.startDate} - {props.endDate}
+          </span>
+          <p className="log--description">{props.description}</p>
+      </div>
+
+</div>
     </section>
   );
 }
